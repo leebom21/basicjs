2ac93f8634599b1e37d4481400e9faf97744d6e0
leebom <d0201d@hanmail.net>
2020-11-04 19:18:11 +0900

    getting the weather API
    날씨 API 사용

--- weather.js
+++ weather.js
@@ -1,5 +1,19 @@
+const weather = document.querySelector(".js-weather");
 
-const COORDS = 'coords';
+
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+const COORDS = "coords";
+
+function getWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY }&units=metric`
+    ).then(function(response){    //서버로부터 데이터가 들어올때까지 기다렸!!!!!
+        return response.json()  //대기상태
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    })
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -15,6 +29,7 @@ function handleGeoSucces(position) {
     longitude // longitude: longitude
    };
    saveCoords(coordsObj);
+   getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -33,6 +48,8 @@ function loadCoords() {
 
     }else {
         // getWeather
+        const parseCoords = JSON.parse(loadedCords);
+       getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
